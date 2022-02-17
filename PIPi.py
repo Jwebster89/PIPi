55cca4f01ed46607f31eb8b23e6a7f76796ea73b
John Webster <john.webster@dpi.nsw.gov.au>
2022-02-17 15:22:41 +1100

    Add summary files of output
    Requested to add a table summary of coverage output for all samples
    combined. Can now be found in the summary output folder.

--- PIPi.py
+++ PIPi.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
-import os, sys, subprocess, argparse
+import os, sys, subprocess, argparse, csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from distutils.spawn import find_executable
 
 pd.options.mode.chained_assignment = None
 
 
 class Pesti():
-		def __init__(self,path,outdir,threads,refs,adapters):
+		def __init__(self,path,outdir,threads,refs,adapters,bed):
 			self.path = path
 			self.outdir = outdir
 			self.threads = threads
 			self.refs = refs
 			self.adapters = adapters
-		
+			self.bed=bed
+
+
 		def run_fastqc(self,path,outdir):
 			print("\n")
 			if not os.path.exists(os.path.join(outdir,"fastqc")):
@@ -36,7 +39,7 @@ class Pesti():
 					print(f"Running bbduk on {os.path.join(path,file)}")
 					subprocess.run(['bbduk.sh','-Xmx2g',f'in1={os.path.join(path,file)}',f'out1={os.path.join(outdir,"1_trimming/",base+".trim.fastq")}',f'stats={os.path.join(outdir,"1_trimming",base+".stats.txt")}',f'ref={adapters}','ktrim=r','k=23','mink=11','hdist=1','qtrim=rl','trimq=10','maq=10','minlen=50','threads=48','tpe','tbo'],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
 
-		def map2ref(self, path, outdir, threads,refs):
+		def map2ref(self, path, outdir, threads,refs,bed):
 			print("\n")
 			if not os.path.exists(os.path.join(outdir,"2_map2ref")):
 				os.mkdir(os.path.join(outdir,"2_map2ref"))
@@ -44,15 +47,14 @@ class Pesti():
 			for file in os.listdir(os.path.join(outdir,"1_trimming")):
 				(base,ext)=os.path.splitext(file)
 				if ext == ".fastq":
-					print(f"Mapping reads from {file} to data/pestivirus_full_genomes.fasta")
+					print(f"Mapping reads from {file} to {refs}")
 					sam=subprocess.Popen(['bwa','mem','-t',str(threads),'PIPi_index_temp',os.path.join(outdir,"1_trimming",file)],stdout=subprocess.PIPE,stderr=subprocess.DEVNULL)
 					subprocess.run(['samtools','sort', '-o',os.path.join(outdir,'2_map2ref',base+'.sort.bam'),'-'],stdin=sam.stdout,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+					print(f"Calculating coverage for {file}\n")
 					subprocess.run(['samtools','coverage', '-o',os.path.join(outdir,"2_map2ref",base+".sort.cov"),os.path.join(outdir,'2_map2ref',base+'.sort.bam')],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
 					subprocess.run(['samtools','index',os.path.join(outdir,'2_map2ref',base+'.sort.bam')])
-					with open(os.path.join(outdir,'2_map2ref',base+'.sort.bedcov'),'wb') as fh:
-						subprocess.run(['samtools','bedcov','data/WG_BVDV.20190405.designed.bed',os.path.join(outdir,'2_map2ref',base+'.sort.bam')],stdout=fh,stderr=subprocess.DEVNULL )
-					print(f"Calculating coverage for {file}\n")
-
+					with open(os.path.join(outdir,'2_map2ref',base+'.sort.bedcov'),'w') as fh:
+						subprocess.run(['samtools','bedcov',bed,os.path.join(outdir,'2_map2ref',base+'.sort.bam')],stdout=fh)
 
 		def output_graph_cov(self,dataframe,outdir,base):
 			if not os.path.exists(os.path.join(outdir,"graphs")):
@@ -89,17 +91,43 @@ class Pesti():
 				(base,ext)=os.path.splitext(file)
 				if ext == ".bedcov":
 					input_data=pd.read_csv(os.path.join(outdir,"2_map2ref",file),sep="\t", header=None)
-					total=input_data[6].sum()
-					percentage=input_data[6] / total *100
+					total=input_data[4].sum()
+					percentage=input_data[4] / total *100
 					input_data["Percentage"]=percentage
 					sorted_data=input_data.loc[(input_data['Percentage'] >= 1)].sort_values(by="Percentage", ascending=False).round({'Percentage':2})
 					sorted_data=sorted_data.rename(columns={3:'rname'})
-					print(sorted_data)
+					# print(sorted_data)
 					sorted_data.to_csv(os.path.join(outdir,'2_map2ref',base+'bedcov_results.csv'))
 					sorted_data['rname']=sorted_data['rname'].str.replace(r'_1.+',"", regex=True)
 					subset_data=sorted_data[['rname','Percentage']]
 					subset_data=subset_data.groupby('rname', as_index=False)['Percentage'].sum()
 					self.output_graph_cov(subset_data,outdir,base+'_bedcov')
+
+		def table_summarise(self,outdir):
+			if not os.path.exists(os.path.join(outdir,"summary")):
+				os.mkdir(os.path.join(outdir,"summary"))
+			top=[['Index','Name', 'Startpos', 'endpos','numreads','covbases','coverage','meandepth','meanbaseq','meannmapq','Percentage','Sample']]
+			all=[['Index','Name', 'Startpos', 'endpos','numreads','covbases','coverage','meandepth','meanbaseq','meannmapq','Percentage','Sample']]
+			for file in os.listdir(os.path.join(outdir,"2_map2ref")):
+				if file[-19:] == "sortcov_results.csv":
+					covdf=pd.read_csv(os.path.join(outdir,"2_map2ref",file))
+					sort_covdf=covdf.sort_values(by="Percentage", ascending=False)
+					for k,row in sort_covdf.iterrows():
+						if k == 0:
+							ls=sort_covdf.loc[k].values.flatten().tolist()
+							ls.append(file[:-25])
+							top.append(ls)
+							all.append(ls)
+						else:
+							ls=sort_covdf.loc[k].values.flatten().tolist()
+							ls.append(file[:-25])
+							all.append(ls)
+			with open(os.path.join(outdir,"summary","Top_hits_per_sample.csv"), "w") as f:
+				writer = csv.writer(f)
+				writer.writerows(top)
+			with open(os.path.join(outdir,"summary","All_hits_per_sample.csv"), "w") as f:
+				writer = csv.writer(f)
+				writer.writerows(all)
 
 		def run(self):
 			if not os.path.exists(self.outdir):
@@ -117,23 +145,25 @@ class Pesti():
 						break
 			self.run_fastqc(self.path, self.outdir)
 			self.trimming(self.path,self.outdir, self.adapters)
-			self.map2ref(self.path, self.outdir, self.threads,self.refs)
+			self.map2ref(self.path, self.outdir, self.threads,self.refs,self.bed)
 			self.parse_cov(self.outdir)
 			self.parse_bedcov(self.outdir)
+			self.table_summarise(self.outdir)
 			#TBC
-			#self.IRMA 
+			#self.IRMA
 			#self.assembly
 
 def main():
 	parser = argparse.ArgumentParser(description="Pestivirus Identification Pipeline", formatter_class=argparse.RawTextHelpFormatter, add_help=False)
 	required = parser.add_argument_group('Required Arguments')
 	optional = parser.add_argument_group('Optional Arguments')
-	
+
 
 	required.add_argument('-p', '--path', type=str, required=True, help="Folder of fastq files from Ion torrent")
 	required.add_argument("-o", "--outdir",type=str, required=True, help="Output directory")
 	required.add_argument("-r", "--reference", type=str, required=True, help="Location of reference Pestivirus sequences")
 	required.add_argument("-a", "--adapters", type=str, required=True, help="Location of the adapter sequences fasta file")
+	required.add_argument("-b", "--bed_file", type=str, required=True, help="Location of the bed file of target regions")
 	optional.add_argument("-t", "--threads", type=str, required=False, default=8, help="Number of threads to use. Default 8")
 	optional.add_argument("-h", "--help", action="help", help="show this help message and exit")
 
@@ -143,9 +173,10 @@ def main():
 	outdir=args.outdir
 	refs=args.reference
 	adapters=args.adapters
+	bed=args.bed_file
 	threads=args.threads
 
-	job=Pesti(path,outdir,threads,refs, adapters)
+	job=Pesti(path,outdir,threads,refs,adapters,bed)
 	job.run()
 
 if __name__ == '__main__':
