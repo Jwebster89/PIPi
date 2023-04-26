6e06ea55d69744241ed69b7201bf8602e270a018
John Webster <john.webster@dpi.nsw.gov.au>
2023-04-27 09:58:30 +1000

    Change functionality for Illumina input

--- PIPi.py
+++ PIPi.py
@@ -21,20 +21,32 @@ class Pesti():
 			if not os.path.exists(os.path.join(outdir,"fastqc")):
 				os.mkdir(os.path.join(outdir,"fastqc"))
 			for file in os.listdir(path):
-				(base,ext)=os.path.splitext(file)
-				if ext == ".fastq":
+				(base, ext) = os.path.splitext(file)
+				if ext in (".fastq", ".fq") and "pass_1" in base:
+					# Find the corresponding R2 file
+					r2_file = base.replace("pass_1", "pass_2") + ext
+					if r2_file not in os.listdir(path):
+						continue
+					r1_path = os.path.join(path, file)
+					r2_path = os.path.join(path, r2_file)
 					print(f"Running fastqc on {os.path.join(path,file)}")
-					subprocess.run(['fastqc','-o',os.path.join(outdir,"fastqc"), os.path.join(path,file)],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+					subprocess.run(['fastqc','-o',os.path.join(outdir,"fastqc"), r1_path, r2_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 		def trimming(self, path, outdir, adapters):
 			print("\n")
 			if not os.path.exists(os.path.join(outdir,"1_trimming")):
 				os.mkdir(os.path.join(outdir,"1_trimming"))
 			for file in os.listdir(path):
-				(base,ext)=os.path.splitext(file)
-				if ext == ".fastq":
+				(base, ext) = os.path.splitext(file)
+				if ext in (".fastq", ".fq") and "pass_1" in base:
+					# Find the corresponding R2 file
+					r2_file = base.replace("pass_1", "pass_2") + ext
+					if r2_file not in os.listdir(path):
+						continue
+					r1_path = os.path.join(path, file)
+					r2_path = os.path.join(path, r2_file)
 					print(f"Running bbduk on {os.path.join(path,file)}")
-					subprocess.run(['bbduk.sh','-Xmx2g',f'in1={os.path.join(path,file)}',f'out1={os.path.join(outdir,"1_trimming/",base+".trim.fastq")}',f'stats={os.path.join(outdir,"1_trimming",base+".stats.txt")}',f'ref={adapters}','ktrim=r','k=23','mink=11','hdist=1','qtrim=rl','trimq=10','maq=10','minlen=50','threads=48','tpe','tbo'],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+					subprocess.run(['bbduk.sh','-Xmx2g',f'in1={r1_path}',f'in2={r2_path}',f'out1={os.path.join(outdir,"1_trimming/",base+".trim.fastq")}',f'out2={os.path.join(outdir,"1_trimming/",r2_file+".trim.fastq")}',f'stats={os.path.join(outdir,"1_trimming",base+".stats.txt")}',f'ref={adapters}','ktrim=r','k=23','mink=11','hdist=1','qtrim=rl','trimq=10','maq=10','minlen=50','threads=48','tpe','tbo'],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
 
 		def map2ref(self, path, outdir, threads,refs):
 			print("\n")
@@ -42,16 +54,23 @@ class Pesti():
 				os.mkdir(os.path.join(outdir,"2_map2ref"))
 			subprocess.run(['bwa','index','-p','PIPi_index_temp',refs],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
 			for file in os.listdir(os.path.join(outdir,"1_trimming")):
-				(base,ext)=os.path.splitext(file)
-				if ext == ".fastq":
-					print(f"Mapping reads from {file} to data/pestivirus_full_genomes.fasta")
-					sam=subprocess.Popen(['bwa','mem','-t',str(threads),'PIPi_index_temp',os.path.join(outdir,"1_trimming",file)],stdout=subprocess.PIPE,stderr=subprocess.DEVNULL)
+				(base, ext) = os.path.splitext(file)
+				if ext in (".fastq", ".fq") and "pass_1" in base:
+					# Find the corresponding R2 file
+					r2_file = base.replace("pass_1", "pass_2") + ext
+					if r2_file not in os.listdir(path):
+						continue
+					idx=base.replace("pass_1","")
+					r1_path=os.path.join(outdir,"1_trimming",file)
+					r2_path=os.path.join(outdir,"1_trimming",r2_file)
+					print(f"Mapping reads from {base} and {r2_file} to {self.refs}")
+					sam=subprocess.Popen(['bwa','mem','-t',str(threads),'PIPi_index_temp',r1_path, r2_path],stdout=subprocess.PIPE,stderr=subprocess.DEVNULL)
 					subprocess.run(['samtools','sort', '-o',os.path.join(outdir,'2_map2ref',base+'.sort.bam'),'-'],stdin=sam.stdout,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
 					subprocess.run(['samtools','coverage', '-o',os.path.join(outdir,"2_map2ref",base+".sort.cov"),os.path.join(outdir,'2_map2ref',base+'.sort.bam')],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
 					subprocess.run(['samtools','index',os.path.join(outdir,'2_map2ref',base+'.sort.bam')])
-					with open(os.path.join(outdir,'2_map2ref',base+'.sort.bedcov'),'wb') as fh:
-						subprocess.run(['samtools','bedcov','data/WG_BVDV.20190405.designed.bed',os.path.join(outdir,'2_map2ref',base+'.sort.bam')],stdout=fh,stderr=subprocess.DEVNULL )
-					print(f"Calculating coverage for {file}\n")
+					# with open(os.path.join(outdir,'2_map2ref',base+'.sort.bedcov'),'wb') as fh:
+					# 	subprocess.run(['samtools','bedcov','data/WG_BVDV.20190405.designed.bed',os.path.join(outdir,'2_map2ref',base+'.sort.bam')],stdout=fh,stderr=subprocess.DEVNULL )
+					# print(f"Calculating coverage for {file}\n")
 
 
 		def output_graph_cov(self,dataframe,outdir,base):
@@ -119,7 +138,7 @@ class Pesti():
 			self.trimming(self.path,self.outdir, self.adapters)
 			self.map2ref(self.path, self.outdir, self.threads,self.refs)
 			self.parse_cov(self.outdir)
-			self.parse_bedcov(self.outdir)
+			# self.parse_bedcov(self.outdir)
 			#TBC
 			#self.IRMA 
 			#self.assembly
